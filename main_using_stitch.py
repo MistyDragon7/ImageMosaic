a3b75e8fb5d8c4dd26fdc19f75034d3f358fbb05
MistyDragon7 <120657456+MistyDragon7@users.noreply.github.com>
2022-12-22 22:49:35 +0530

    Update main_using_stitch.py

--- main_using_stitch.py
+++ main_using_stitch.py
@@ -1,25 +1,24 @@
 import numpy
-import cv2
-import glob
+import cv2				#OpenCV module, our main tool for the task.
+import glob				#glob can be used to open paths of multiple files and import them into a list simultaneously, like a mass select.
 
-img_names = glob.glob('./*.jpg')
+img_names = glob.glob('./*.jpg')	#Import all jpg images in the folder and adding their names to a list
 images = []
-print(img_names[0])
 for image in img_names:
-	img = cv2.imread(image)
-	images.append(img)
-	cv2.imshow("Image", img)
-	cv2.waitKey(0)
+	img = cv2.imread(image)		#imported the image into a variable
+	images.append(img)		#imported image into a list for mass use.
+	cv2.imshow("Image", img)	#image displayed for verififcation
+	cv2.waitKey(0)			
 
-Mosaic = cv2.Stitcher_create()
+Mosaic = cv2.Stitcher_create()		#initialised the stitcher module as variable Mosaic.
 
-error, mosaic = Mosaic.stitch(images)
+error, mosaic = Mosaic.stitch(images)	#final image is stored as mosaic.
 
 if not error:
 
-	cv2.imwrite("StitchedOutput.png", mosaic)
-	cv2.imshow("Mosaic", mosaic)
-	cv2.waitKey(0)
+	cv2.imwrite("StitchedOutput.png", mosaic)	#Storing the mosaic into the same folder.
+	cv2.imshow("Mosaic", mosaic)	#Showing the final image.
+	cv2.waitKey(0)			#Waits for keypress and then kills the window.
 
 else:
 	print("Mosaic failed")
